e05a0a5cb5707a15b05da4b2dc18adeb402bfd56
paperen <paperen@gmail.com>
2012-10-17 22:49:35 +0800

    视图文件调整
    Signed-off-by: paperen <paperen@gmail.com>

--- view/default/hg_footer.php
+++ view/default/hg_footer.php
@@ -1,16 +1,6 @@
 <?php if(!defined('INRUN')) exit(); ?>
-    </div>
-</div>
-<!-- main -->
-<div class="c"></div>
-<!-- footer -->
-<div class="footer bg">
-	<div class="inner">
-    	<p class="description"><?php echo $this->foottxt; ?></p>
-        <div class="sign"><?php echo $this->copration; ?></div>
-    </div>
-</div>
-<!-- footer -->
-<div class="errortip" id="errortip"></div>
-</body>
+		</div>
+		<?php echo $this->js( 'script_jquery.js' ); ?>
+		<?php echo $this->js( 'script_hackgame.js' ); ?>
+	</body>
 </html>
--- view/default/hg_header.php
+++ view/default/hg_header.php
@@ -1,34 +1,22 @@
-<?php if(!defined('INRUN')) exit(); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $this->charset; ?>" />
-<title><?php echo $this->title; ?></title>
-<meta name="keywords" content="<?php echo $this->keywords; ?>"/>
-<meta name="description" content="<?php echo $this->description; ?>"/>
-<?php $this->import( 'default', 'css' ); ?>
-<?php $this->import( 'script_jquery', 'js' ); ?>
-<?php $this->import( 'script_engine', 'js' ); ?>
-<?php $this->import( 'script_share', 'js' ); ?>
-</head>
-
-<body>
-<!-- header -->
-<div class="header bg">
-	<div class="inner">
-    	<a href="<?php echo $this->siteurl; ?>" class="logo"></a>
-        <a href="javascript:void(0);" class="share" title="分享到" id="switch_share"></a>
-        <ul class="share_inner" id="share_box">
-        	<li><a href="javascript:void(0);" class="icon sina_icon" id="share_sina"></a></li>
-            <li><a href="javascript:void(0);" class="icon renren_icon" id="share_renren"></a></li>
-            <li><a href="javascript:void(0);" class="icon tengxun_icon" id="share_qq"></a></li>
-            <li><a href="javascript:void(0);" class="icon douban_icon" id="share_douban"></a></li>
-            <li><a href="javascript:void(0);" class="icon twitter_icon" id="share_twitter"></a></li>
-        </ul>
-        <div class="c"></div>
-    </div>
-</div>
-<!-- header -->
-<!-- main -->
-<div class="main">
-	<div class="inner">
+<?php if ( !defined( 'INRUN' ) ) exit();?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo $title; ?></title>
+		<?php echo $this->css( 'bootstrap/css/bootstrap.min.css' ); ?>
+		<?php echo $this->css( 'welcome.css' ); ?>
+	</head>
+	<body>
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="brand" href="#"></a>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
